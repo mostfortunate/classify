e6128a43c8dbd605783b18b4ccb38b5f08566687
Asaad Ahmed <asaadbinahmed@gmail.com>
2025-11-18 15:08:00 -0500

    backend project init

---
+++ backend/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("backend is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
